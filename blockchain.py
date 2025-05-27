6c438408597c3c21750c85ad9a7a92abdab917d3
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-05-27 11:10:19 +0800

    complete new_block, hash, and last_block

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,25 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        self.new_block(previous_hash='1', proof=100)
 
     # create a new block and add it to the chain
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     # create a new transaction and add it to the list of transactions
     def new_transaction(self, sender_address, recipient_address, amount):
@@ -19,9 +33,10 @@ class Blockchain(object):
     # hash a block
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     # get the last block in the chain
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
