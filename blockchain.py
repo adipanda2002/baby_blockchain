4503c306f0802206ac12dd0262a867553d071256
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-05-27 11:25:57 +0800

    add boilerplates for endpoints

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,9 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
 from uuid import uuid4
+from flask import Flask, jsonify
 
 class Blockchain(object):
     def __init__(self):
@@ -55,3 +57,25 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "Return a new Block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "Return a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
