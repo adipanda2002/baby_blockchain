14e9d282866a006f23c8684a47bf44fb6a2d421e
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-05-27 10:59:13 +0800

    add blueprint for blockchain class

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    # create a new block and add it to the chain
+    def new_block(self):
+        pass
+
+    # create a new transaction and add it to the list of transactions
+    def new_transaction(self):
+        pass
+
+    # hash a block
+    @staticmethod
+    def hash(block):
+        pass
+
+    # get the last block in the chain
+    @property
+    def last_block(self):
+        pass
