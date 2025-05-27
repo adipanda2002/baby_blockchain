4084690b0af8246b188b42f71e1b8948b4db8da9
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-05-27 11:03:23 +0800

    complete new_transaction method

--- blockchain.py
+++ blockchain.py
@@ -8,8 +8,13 @@ class Blockchain(object):
         pass
 
     # create a new transaction and add it to the list of transactions
-    def new_transaction(self):
-        pass
+    def new_transaction(self, sender_address, recipient_address, amount):
+        self.current_transactions.append({
+            'sender': sender_address,
+            'recipient': recipient_address,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
 
     # hash a block
     @staticmethod
