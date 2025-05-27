d3904884e1eae21c475e7eec63753aa90847d127
Aditya Ghosh <adityaghosh2002@gmail.com>
2025-05-27 11:28:31 +0800

    complete POST endpoint

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,7 @@ import json
 from textwrap import dedent
 from time import time
 from uuid import uuid4
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 class Blockchain(object):
     def __init__(self):
@@ -67,7 +67,13 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "Return a new transaction"
+    values = request.get_json()
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
